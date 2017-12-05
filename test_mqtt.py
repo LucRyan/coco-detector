ef2617c01197d22dc8a4d739d3447bfce04590ba
Ryan Wang <luckin89@gmail.com>
2017-12-05 10:56:07 -0800

    changed cool down.

--- test_mqtt.py
+++ test_mqtt.py
@@ -1,12 +1,18 @@
 import time
+import sys
 from utils.mqtt_simple_client import MqttClient
 
-COOLDOWN = 120
+COOL_DOWN = 120
+
+
+def stop():
+    sys.exit()
+
 
 if __name__ == '__main__':
     client = MqttClient().connect()
+    client.eventSystem.on(client.EVENT_TURNOFF, stop)
 
-    while True:  # fps._numFrames < 120
+    while True:
         client.publish()
-        time.sleep(COOLDOWN)
-
+        time.sleep(COOL_DOWN)

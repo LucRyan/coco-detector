a4ab1697698c721e00c3a697e28fb8fdc7c54519
Ryan Wang <luckin89@gmail.com>
2017-12-05 17:34:27 -0800

    Added more robust wait functions.

--- coco_detector_app.py
+++ coco_detector_app.py
@@ -1,5 +1,21 @@
 from utils.mqtt_simple_client import MqttClient
 from utils.object_detect import ObjectDetect
+from threading import Event
+
+kill = Event()
+
+
+def main():
+    while not kill.is_set():
+        if kill.is_set():
+            break
+
+    print("All done!")
+    # perform any cleanup here
+
+
+def stop():
+    kill.set()
 
 
 if __name__ == '__main__':
@@ -10,7 +26,12 @@ if __name__ == '__main__':
 
     # Register Events
     client.eventSystem.on(client.EVENT_TURNOFF, objectDetect.stop)
+
     # TODO make this work! seems tensorflow can only runs on main thread.
+    # TODO *** Terminating app due to uncaught exception 'NSInternalInconsistencyException', reason: '
+    # TODO [NSUndoManager(NSInternal) _endTopLevelGroupings] is only safe to invoke on the main thread.'
+    # client.eventSystem.on(client.EVENT_TURNOFF, stop)
     # client.eventSystem.on(client.EVENT_TURNON, objectDetect.start)
+    # main()
 
     objectDetect.start()
--- test_mqtt.py
+++ test_mqtt.py
@@ -1,18 +1,26 @@
-import time
-import sys
+from threading import Event
 from utils.mqtt_simple_client import MqttClient
 
-COOL_DOWN = 120
+# The cooldown in mqtt client is 30s
+COOL_DOWN = 120.0
+kill = Event()
+
+
+def main():
+    while not kill.is_set():
+        client.publish()
+        kill.wait(COOL_DOWN)
+
+    print("All done!")
+    # perform any cleanup here
 
 
 def stop():
-    sys.exit()
+    kill.set()
 
 
 if __name__ == '__main__':
     client = MqttClient().connect()
     client.eventSystem.on(client.EVENT_TURNOFF, stop)
 
-    while True:
-        client.publish()
-        time.sleep(COOL_DOWN)
+    main()
